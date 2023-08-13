63cf229b3253f6c00f6e6b9408731020fee92df7
amin-mirzakuchaki-negah <amin.mirzakuchaki.negah@gmail.com>
2023-08-13 09:52:20 +0330

    Added latest changes

--- lib/core/error/errors.dart
+++ lib/core/error/errors.dart
@@ -1,3 +1,6 @@
 import 'package:equatable/equatable.dart';
 
-abstract class Failure extends Equatable {}
+abstract class Failure extends Equatable {
+  // final String message;
+  // Failure({required this.message});
+}
